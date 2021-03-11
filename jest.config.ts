257be000cc1028eb0dd71ae170b50d049bd881b5
hagevvashi <hagevvashi@gmail.com>
2021-03-11 22:30:59 +0900

    chore: some code change

--- jest.config.ts
+++ jest.config.ts
@@ -3,17 +3,16 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   clearMocks: true,
   coverageDirectory: "<rootDir>/coverage/",
-  testMatch: ["**/?(*.)+(test).ts?(x)", "**/?(*.)+(test-d).ts?(x)"],
+  testMatch: ["**/?(*.)+(test-d).ts?(x)"],
   transform: {
     "^.+\\.test-d.tsx?$": "dts-jest/transform",
-    "^.+\\.[jt]sx?$": "babel-jest"
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
   globals: {
     _dts_jest_: {
       compiler_options: "./tsconfig.json",
-      transpile: false
-    }
-  }
+      transpile: false,
+    },
+  },
 };
 export default config;
